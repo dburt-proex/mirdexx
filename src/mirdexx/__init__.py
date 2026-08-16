0341ef2221e6c7eeab4c312e117911f6f71118a3
Drew Burt <drewburt4@gmail.com>
2026-08-16 16:37:05 -0500

    feat: export event ledger API

--- src/mirdexx/__init__.py
+++ src/mirdexx/__init__.py
@@ -2,12 +2,17 @@
 
 from .config import AppConfig
 from .database import bootstrap_database
+from .event_ledger import ContextUseDenied, EventIntegrityError, EventLedger, NormalizedEvent
 from .source_registry import BoundaryDenied, SourceRegistry, WatchedSource
 
 __all__ = [
     "AppConfig",
     "bootstrap_database",
     "BoundaryDenied",
+    "ContextUseDenied",
+    "EventIntegrityError",
+    "EventLedger",
+    "NormalizedEvent",
     "SourceRegistry",
     "WatchedSource",
 ]
